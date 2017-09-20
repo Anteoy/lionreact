6a2f8bc5425eab615912fe5499dba3fa06ad66bb
anteoy <anteoy@gmail.com>
2017-09-20 23:46:25 +0800

    add lancher

---
+++ src/components/lancher/Lancher.js
@@ -0,0 +1,27 @@
+/* eslint-disable */
+import React from 'react';
+import { connect } from 'dva';
+import { Button } from 'antd';
+import styles from './Lancher.css';
+
+class PnoteLancher extends React.Component {
+  state = {
+    title: '',
+    content: '',
+  };
+
+  render() {
+    return (
+    <div >
+      <div ><input id="title" placeholder="标题" /></div>
+      <div ><textarea id="content" rows="60" cols="200" placeholder="正文"></textarea></div>
+      <div ><button >提交</button></div>
+    </div>
+    );
+  }
+}
+function mapStateToProps(state) {
+  return state;
+}
+
+export default connect(mapStateToProps)(PnoteLancher);
--- src/router.js
+++ src/router.js
@@ -2,16 +2,16 @@ import React from 'react';
 import { Router, Route } from 'dva/router';
 // import IndexPage from './routes/IndexPage';
 import PnoteIndex from './components/login/Login';
+import PnoteLancher from './components/lancher/Lancher';
 
 import Login from './routes/Login.js';
 
 function RouterConfig({ history }) {
   return (
     <Router history={history}>
-      <Route path="/" component={PnoteIndex} />
       <Route path="/login_v0.0.1" component={Login} />
       {/* 登录页 */}
-      <Route path="/" component={PnoteIndex} />
+      <Route path="/" component={PnoteLancher} />
       <Route path="/test" component={PnoteIndex} />
     </Router>
   );
